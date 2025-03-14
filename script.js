35e548012f379ce7e7acfaa92ef09e08e6756c6b
Necronyte <tolgakagan18@gmail.com>
2025-03-14 18:10:50 +0300

    update

--- script.js
+++ script.js
@@ -5,17 +5,14 @@ document.addEventListener("DOMContentLoaded", function() {
         const answerContainer = question.nextElementSibling;
         const toggleIcon = question.querySelector("img");
 
-        // Başlangıçta tüm cevapları gizle
         answerContainer.style.maxHeight = "0";
         answerContainer.style.opacity = "0";
         answerContainer.style.overflow = "hidden";
         toggleIcon.src = "assets/images/icon-plus.svg";
 
-        // Tıklama olayını ekle
         question.addEventListener("click", function() {
             const isOpen = answerContainer.style.maxHeight !== "0px";
 
-            // Önce tüm cevapları kapat
             document.querySelectorAll(".answer-container").forEach(answer => {
                 answer.style.maxHeight = "0";
                 answer.style.opacity = "0";
@@ -26,9 +23,8 @@ document.addEventListener("DOMContentLoaded", function() {
                 icon.src = "assets/images/icon-plus.svg";
             });
 
-            // Eğer zaten açıksa kapalı bırak
             if (!isOpen) {
-                answerContainer.style.maxHeight = "200px"; // İçeriğe uygun yükseklik
+                answerContainer.style.maxHeight = "200px"; 
                 answerContainer.style.opacity = "1";
                 answerContainer.style.overflow = "visible";
                 toggleIcon.src = "assets/images/icon-minus.svg";
